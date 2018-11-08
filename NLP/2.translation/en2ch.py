017374a4d26f105fea7c85538d7f99768ac09bee
speecher <hit_master@163.com>
2018-11-08 14:33:24 +0800

    20181108

--- NLP/2.translation/en2ch.py
+++ NLP/2.translation/en2ch.py
@@ -2,132 +2,151 @@
 with open('cmn.txt', 'r', encoding='utf-8') as f:
     data = f.read()
 data = data.split('\n')
-data = data[:5000]
-print(data[-1:])
+data = data[:100]
+print(data[-5:])
+
 
 # 分割英文数据和中文数据
 en_data = [line.split('\t')[0] for line in data]
-ch_data = [line.split('\t')[1] for line in data]
-print('英文数据:', en_data[:10])
-print('中文数据:', ch_data[:10])
+ch_data = ['\t' + line.split('\t')[1] + '\n' for line in data]
+print('英文数据:\n', en_data[:10])
+print('\n中文数据:\n', ch_data[:10])
 
 # 分别生成中英文字典
 en_vocab = set(''.join(en_data))
-id2en = ['__PAD__', '__UNK__'] + list(en_vocab)
+id2en = list(en_vocab)
 en2id = {c:i for i,c in enumerate(id2en)}
 
+# 分别生成中英文字典
 ch_vocab = set(''.join(ch_data))
-id2ch = ['__PAD__', '__UNK__', '__GO__', '__EOS__'] + list(ch_vocab)
+id2ch = list(ch_vocab)
 ch2id = {c:i for i,c in enumerate(id2ch)}
 
-print('英文字典:\n', en2id)
-print('中文字典共计:', len(ch2id))
+print('\n英文字典:\n', en2id)
+print('\n中文字典共计\n:', ch2id)
 
+
+
+# 利用字典，映射数据
 en_num_data = [[en2id[en] for en in line ] for line in en_data]
-ch_num_data = [[ch2id['__GO__']] + [ch2id[ch] for ch in line] for line in ch_data]
-de_num_data = [[ch2id[ch] for ch in line] + [ch2id['__EOS__']] for line in ch_data]
-print(en_num_data[:5])
-print(ch_num_data[:5])
-print(de_num_data[:5])
+ch_num_data = [[ch2id[ch] for ch in line] for line in ch_data]
+de_num_data = [[ch2id[ch] for ch in line][1:] for line in ch_data]
+
+print('char:', en_data[1])
+print('index:', en_num_data[1])
+
+
 
 import numpy as np
 
+# 获取输入输出端的最大长度
 max_encoder_seq_length = max([len(txt) for txt in en_num_data])
 max_decoder_seq_length = max([len(txt) for txt in ch_num_data])
-print(max_encoder_seq_length)
-print(max_decoder_seq_length)
+print('max encoder length:', max_encoder_seq_length)
+print('max decoder length:', max_decoder_seq_length)
 
-
-encoder_input_data = [line + [0] * (max_encoder_seq_length-len(line)) for line in en_num_data]
-decoder_input_data = [line + [0] * (max_decoder_seq_length-len(line)) for line in ch_num_data]
-decoder_output_data = [line + [0] * (max_decoder_seq_length-len(line)) for line in de_num_data]
+# 将数据进行onehot处理
+encoder_input_data = np.zeros((len(en_num_data), max_encoder_seq_length, len(en2id)), dtype='float32')
+decoder_input_data = np.zeros((len(ch_num_data), max_decoder_seq_length, len(ch2id)), dtype='float32')
 decoder_target_data = np.zeros((len(ch_num_data), max_decoder_seq_length, len(ch2id)), dtype='float32')
-for i in range(len(ch_num_data)):
-    for j in range(max_decoder_seq_length):
-        decoder_target_data[i,j,decoder_output_data[i][j]] = 1
 
-print(decoder_target_data.shape)
+for i in range(len(ch_num_data)):
+    for t, j in enumerate(en_num_data[i]):
+        encoder_input_data[i, t, j] = 1.
+    for t, j in enumerate(ch_num_data[i]):
+        decoder_input_data[i, t, j] = 1.
+    for t, j in enumerate(de_num_data[i]):
+        decoder_target_data[i, t, j] = 1.
+
+print('index data:\n', en_num_data[1])
+print('one hot data:\n', encoder_input_data[1])
+
+
 
 # =======预定义模型参数========
 EN_VOCAB_SIZE = len(en2id)
 CH_VOCAB_SIZE = len(ch2id)
-EPOCHS = 10
 HIDDEN_SIZE = 256
+
+LEARNING_RATE = 0.003
+BATCH_SIZE = 100
+EPOCHS = 200
+
+
 
 # ======================================keras model==================================
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense, Dropout, Embedding, Masking
-from keras import regularizers
+from keras.layers import Input, LSTM, Dense, Embedding
 from keras.optimizers import Adam
 import numpy as np
 
 # ==============encoder=============
-encoder_inputs = Input(shape=(None,))
-emb_inp = Embedding(output_dim=HIDDEN_SIZE, input_dim=EN_VOCAB_SIZE, mask_zero=True)(encoder_inputs)
-encoder_h1, encoder_state_h1, encoder_state_c1 = LSTM(HIDDEN_SIZE, activation='relu', return_sequences=True, return_state=True, dropout=0.2)(emb_inp)
-encoder_h2, encoder_state_h2, encoder_state_c2 = LSTM(HIDDEN_SIZE, activation='relu', return_state=True, dropout=0.2)(encoder_h1)
-encoder_state = [[encoder_state_h1, encoder_state_c1],[encoder_state_h2, encoder_state_c2]]
+encoder_inputs = Input(shape=(None, EN_VOCAB_SIZE))
+#emb_inp = Embedding(output_dim=HIDDEN_SIZE, input_dim=EN_VOCAB_SIZE)(encoder_inputs)
+encoder_h1, encoder_state_h1, encoder_state_c1 = LSTM(HIDDEN_SIZE, return_sequences=True, return_state=True)(encoder_inputs)
+encoder_h2, encoder_state_h2, encoder_state_c2 = LSTM(HIDDEN_SIZE, return_state=True)(encoder_h1)
+
+
 
 # ==============decoder=============
-decoder_inputs = Input(shape=(None, ))
+decoder_inputs = Input(shape=(None, CH_VOCAB_SIZE))
 
-emb_target = Embedding(output_dim=HIDDEN_SIZE, input_dim=CH_VOCAB_SIZE, mask_zero=True)(decoder_inputs)
-lstm1 = LSTM(HIDDEN_SIZE, activation='relu', return_sequences=True, return_state=True, dropout=0.2)
-lstm2 = LSTM(HIDDEN_SIZE, activation='relu', return_sequences=True, return_state=True, dropout=0.2)
+#emb_target = Embedding(output_dim=HIDDEN_SIZE, input_dim=CH_VOCAB_SIZE, mask_zero=True)(decoder_inputs)
+lstm1 = LSTM(HIDDEN_SIZE, return_sequences=True, return_state=True)
+lstm2 = LSTM(HIDDEN_SIZE, return_sequences=True, return_state=True)
 decoder_dense = Dense(CH_VOCAB_SIZE, activation='softmax')
 
-decoder_h1, _, _ = lstm1(emb_target, initial_state=encoder_state[0])
-decoder_h2, _, _ = lstm2(decoder_h1, initial_state=encoder_state[1])
+decoder_h1, _, _ = lstm1(decoder_inputs, initial_state=[encoder_state_h1, encoder_state_c1])
+decoder_h2, _, _ = lstm2(decoder_h1, initial_state=[encoder_state_h2, encoder_state_c2])
 decoder_outputs = decoder_dense(decoder_h2)
 
-batch_size = 64
-epochs = 20
+
 
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+opt = Adam(lr=LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0)
+          batch_size=BATCH_SIZE,
+          epochs=EPOCHS,
+          validation_split=0.)
 
 # Save model
-model.save('s2s.h5')
+#model.save('s2s.h5')
 
-encoder_model = Model(encoder_inputs, [encoder_state_h1, encoder_state_c1,encoder_state_h2, encoder_state_c2])
 
+
+# encoder模型和训练相同
+encoder_model = Model(encoder_inputs, [encoder_state_h1, encoder_state_c1, encoder_state_h2, encoder_state_c2])
+
+# 预测模型中的decoder的初始化状态需要传入新的状态
 decoder_state_input_h1 = Input(shape=(HIDDEN_SIZE,))
 decoder_state_input_c1 = Input(shape=(HIDDEN_SIZE,))
 decoder_state_input_h2 = Input(shape=(HIDDEN_SIZE,))
 decoder_state_input_c2 = Input(shape=(HIDDEN_SIZE,))
 
-decoder_h1, state_h1, state_c1 = lstm1(emb_target, initial_state=[decoder_state_input_h1, decoder_state_input_c1])
+# 使用传入的值来初始化当前模型的输入状态
+decoder_h1, state_h1, state_c1 = lstm1(decoder_inputs, initial_state=[decoder_state_input_h1, decoder_state_input_c1])
 decoder_h2, state_h2, state_c2 = lstm2(decoder_h1, initial_state=[decoder_state_input_h2, decoder_state_input_c2])
 decoder_outputs = decoder_dense(decoder_h2)
 
 decoder_model = Model([decoder_inputs, decoder_state_input_h1, decoder_state_input_c1, decoder_state_input_h2, decoder_state_input_c2], 
                       [decoder_outputs, state_h1, state_c1, state_h2, state_c2])
 
-print(encoder_input_data[1])
-print(decoder_input_data[1])
-print(decoder_output_data[1])
-print(''.join([id2en[i] for i in encoder_input_data[1]]))
-print(''.join([id2ch[i] for i in decoder_input_data[1]]))
-print(''.join([id2ch[i] for i in decoder_output_data[1]]))
 
-for k in range(50):
-    test_data = encoder_input_data[k]
-    h1, c1,h2, c2 = encoder_model.predict(test_data)
-    condition = True
+for k in range(40,50):
+    test_data = encoder_input_data[k:k+1]
+    h1, c1, h2, c2 = encoder_model.predict(test_data)
+    target_seq = np.zeros((1, 1, CH_VOCAB_SIZE))
+    target_seq[0, 0, ch2id['\t']] = 1
     outputs = []
-    decoder_input = [2]
-    while condition:
-        output, h1, c1, h2, c2 = decoder_model.predict([decoder_input, h1, c1, h2, c2])
-        outputs.append(np.argmax(output))
-        decoder_input = [np.argmax(output)]
-        if decoder_input == 3 or len(outputs) > 20: condition = False
-    print(''.join([id2en[i] for i in test_data]))
-    print(''.join([id2ch[i] for i in decoder_output_data[k]]))
+    while True:
+        output_tokens, h1, c1, h2, c2 = decoder_model.predict([target_seq, h1, c1, h2, c2])
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        outputs.append(sampled_token_index)
+        target_seq = np.zeros((1, 1, CH_VOCAB_SIZE))
+        target_seq[0, 0, sampled_token_index] = 1
+        if sampled_token_index == ch2id['\n'] or len(outputs) > 20: break
+    
+    print(en_data[k])
     print(''.join([id2ch[i] for i in outputs]))
-   
--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # my_deep_project
+
+项目地址：[https://github.com/audier/my_deep_project](https://github.com/audier/my_deep_project)
+最近准备整理一些使用深度模型实现的项目，作为最近工作学习的一个整理，因为之前学习太不系统，有些杂乱，希望这个项目系列完成之后能够对自己的学习有一个总结，有些新的收获。
+**该系列项目尽量都使用TensorFlow、keras分别进行实现，其内容包括：**
+- 深度学习项目规范
+- 深度模型入门项目
+- 语音识别应用
+- 自然语言处理
+- 图像识别
+- GAN
+- ...
+整个系列项目及文档将在12月之前完成，先立个flag，希望自己好好整理，能够产出一些真正有用的东西，为他人所用。
+PS: 之前的一个语音识别的项目已经都没有怎么去维护整理了，一方面是最近确实是又被安排去做其他的事情了，没有连续下来，另一方面是发现自己对于这些框架的基础确实薄弱，限制了自己的创造力。这次希望整理这些东西，能够扎实自己的基础吧。
+
+## 0. 深度学习项目规范
+ 我认为一个完整的深度学习项目应该包含如下所示几个方面：
+1. **项目背景**：项目背景是什么，完成什么任务？
+2. **项目数据**：项目数据是如何获取的，数据都包含什么，输入什么输出什么？
+4. **数据处理**：将获得的数据数字化，处理为能够喂进模型的形式。也包括：数据增强、去除脏数据等
+5. **模型选择与建模**：核心，也是深度学习框架要实现的主体
+6. **评估准则与效果**：评价模型好坏的标准是什么，如何通过评价标准评估模型效果
+7. **模型优化与提升**：哪些方面还可以提升模型的性能？
+
+按照这个结构处理深度学习的任务，会更加规范更易提升，后续我将严格按照这个流程执行自己的项目以及整理回溯。
+ 
+## 1. 深度学习入门项目 
+文档地址：https://blog.csdn.net/chinatelecom08/article/details/83413623#_5
+项目地址：https://github.com/audier/my_deep_project/tree/master/basic_deep_model
+1.	**TensorFlow实现mnist分类**
+	- DNN示例
+	- CNN示例
+	- RNN示例
+2.	**keras实现mnist分类**
+	- DNN示例
+	- CNN示例
+	- RNN示例
+
+## 2. 自然语言处理
+1. **文章自动生成**
+文档地址：https://blog.csdn.net/chinatelecom08/article/details/83654602
+项目地址：https://github.com/audier/my_deep_project/tree/master/NLP/1.moyan_novel
+	- lstm : tensorflow
+	- lstm : keras
+2. **翻译系统**
+文档地址：https://blog.csdn.net/chinatelecom08/article/details/83860179
+项目地址：https://github.com/audier/my_deep_project/tree/master/NLP/2.translation
+	- seq2seq (tensorflow)
+	- seq2seq + attention (tensorflow)
+4. **对话系统**
+	- seq2seq +attention (keras)
+5. **输入法系统**
+	- CBHG (tensorflow)
+	- CBHG (keras)
+
+## 3. 语音识别 
+文档地址：https://blog.csdn.net/chinatelecom08/article/details/82557715
+项目地址：https://github.com/audier/my_ch_speech_recognition
+1. CTC + RNN
+2. CTC + CNN 
+3. seq2seq +attention (keras)
+
+## 4. 图像识别
+1. 目标检测
+2. 风格迁移
+3. 文本生成
+## 5. GAN
+1. mnist图像生成
+	- TensorFlow
+	- keras
+
+
+这是给自己立的一个flag，当这些都完成后，希望能够更好的理解这些深度框架，以及一些细节理论。
+当然，也有很大的可能完不成这些任务，图像相关的任务自己也只做过mnist，其他的也不是很了解。但是不管怎么说，还是希望能够把列出来的这些任务都能做一遍，更好的理解深度模型在这些任务中是如何发挥作用的。加油。
